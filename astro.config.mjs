582959109b3b7692e3a19e5c9a99d27ddcf9f364
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2022-09-18 10:05:20 +0700

    clean code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-
 import image from "@astrojs/image";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://astro-multiverse.netlify.app",
   integrations: [image()]
