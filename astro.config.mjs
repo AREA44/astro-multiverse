0ca2c542a62c95a3237161b9b7aef473da52701c
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2022-09-22 03:12:37 +0700

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import image from "@astrojs/image";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-multiverse.netlify.app",
-  integrations: [image()]
+  integrations: [image(), partytown()]
 });
