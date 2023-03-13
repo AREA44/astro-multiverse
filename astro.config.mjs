765b46a2ae9a95d9ad2bdc714e15a52b1c1d8e01
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-03-13 18:29:41 +0700

    Move to v2 (#89)
    * move to v2
    
    * fix astro sitemap
    
    * remove aspectRatio
    
    * add height
    
    * done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
+// https://astro.build/config
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://astro-multiverse.netlify.app",
+  experimental: {
+    assets: true,
+  },
   integrations: [sitemap()],
 });
