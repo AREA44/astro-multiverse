ace60aba067a4930f608403ba2fa3802b088d6b3
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-03-13 17:48:21 +0700

    move to v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config';
-import image from "@astrojs/image";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-multiverse.netlify.app",
-  integrations: [image()]
+  experimental: {
+    assets: true,
+  },
+  integrations: [sitemap()],
 });
