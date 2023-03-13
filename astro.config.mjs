4e72f0cd911eea09202b66b65927ad7bbc3413bf
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-03-13 18:22:06 +0700

    remove aspectRatio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
+// https://astro.build/config
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://astro-multiverse.netlify.app",
   experimental: {
