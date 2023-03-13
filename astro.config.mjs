1ff383d4051864428adf076946b531409b3407af
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-03-13 18:09:35 +0700

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from 'astro/config';
-import image from "@astrojs/image";
+import { defineConfig } from "astro/config";
+import image from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-multiverse.netlify.app",
-  integrations: [image()]
+  integrations: [sitemap()],
 });
