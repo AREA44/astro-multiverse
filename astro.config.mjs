22d9a8e38293828d9468edd5fedaddaf4293c657
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-03-13 18:10:32 +0700

    fix astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import image from "@astrojs/sitemap";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
