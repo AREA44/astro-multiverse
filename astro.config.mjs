b3307ca082d9fa9e20ff6b51c775b1cb943b5959
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-07-07 17:33:47 +0700

    fix #93

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-multiverse.netlify.app',
-  integrations: [compress(), critters()],
+  integrations: [critters(), compress({ svg: false })],
 });
