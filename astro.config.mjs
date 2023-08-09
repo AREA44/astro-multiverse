2cffb9187c1244fa261cf924899c7f52b6bb1572
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-08-09 15:25:47 +0700

    fixed #99

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import compress from 'astro-compress'
-import critters from 'astro-critters'
+import Compress from 'astro-compress'
+import Critters from 'astro-critters'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-multiverse.netlify.app',
-  integrations: [critters(), compress({ svg: false })],
+  integrations: [Critters(), Compress({ svg: false })],
 })
