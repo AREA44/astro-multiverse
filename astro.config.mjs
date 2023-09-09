1dffacdf55ebf0fdaef6129ea5a8e4b535e42925
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-09-09 23:08:39 +0700

    improve: improve CLS

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-multiverse.netlify.app',
+  site: 'https://astro-multiverse.vercel.app',
 })
