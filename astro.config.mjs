9dbc7b6a37ea6741fee86a1ed7867465b4732d1a
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2024-09-05 00:30:39 +0700

    try to optimize font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,22 @@
 import { defineConfig } from 'astro/config'
+import { FontaineTransform } from 'fontaine'
+
+const VERCEL_PREVIEW_SITE =
+  process.env.VERCEL_ENV !== 'production' &&
+  process.env.VERCEL_URL &&
+  `https://${process.env.VERCEL_URL}`
+
+const site = VERCEL_PREVIEW_SITE || 'https://torn4dom4n.github.io'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-multiverse.vercel.app',
+  site: process.env.CI ? site : 'http://localhost:4321',
+  vite: {
+    plugins: [
+      FontaineTransform.vite({
+        fallbacks: ['Arial'],
+        resolvePath: (id) => new URL(`./public${id}`, import.meta.url),
+      }),
+    ],
+  },
 })
